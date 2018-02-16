5c0213ac3a7588a28a4e3b91860a1027e2819b3f
Unknown <djh1997@hotmail.com>
2018-02-16 12:30:17 +0000

    remove uneeded comments and include timing and scale factor

--- design/vision/shades.py
+++ design/vision/shades.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -39,6 +19,8 @@ WIDTH = 128
 HEIGHT = 160
 SPEED_HZ = 640000000
 
+scaleFactor=4
+
 # Raspberry Pi configuration.
 DC = 24
 RST = 25
@@ -57,28 +39,42 @@ disp = TFT.ST7735(
 
 camera = picamera.PiCamera()
 camera.color_effects=(128,128)
-camera.resolution = (160,128)
+camera.resolution = (160*scaleFactor,128*scaleFactor)
 camera.start_preview()
+
+disp.begin()
+draw = disp.draw()
 
 for k in range(100):
     print(k)
-    start=time()
-    camera.capture('image1.jpg')
-    img1=imread('image1.jpg',as_grey=True)
+    timer=[]
+    timer.append(time())#t1
 
-    blobs_doh = blob_doh(img1, max_sigma=30, threshold=.01)
+    camera.capture('image1.jpg')#0.5
+
+    timer.append(time())#t2
+
+    img1=imread('image1.jpg',as_grey=True)#0.06
+
+    timer.append(time())#t3
+
+    blobs_doh = blob_doh(img1, max_sigma=30, threshold=.01)#.44
+
+    timer.append(time())#t4
+
     points=[]
-    for i in range(len(blobs_doh)):
-        points.append([blobs_doh[i][0],blobs_doh[i][1],blobs_doh[i][1]/2,50])
+    for i in range(len(blobs_doh)):#0.0002
+        points.append([blobs_doh[i][0]/scaleFactor,blobs_doh[i][1]/scaleFactor,(blobs_doh[i][1]/2)/scaleFactor,50])
 
-    print(points)
-    print('@')
-    # Initialize display.
-    disp.begin()
-    disp.clear((255,255,255))
-    draw = disp.draw()
+    timer.append(time())#t6
 
-    for i in range(0,len(points)):
+    disp.clear((255,255,255))#0.036
+
+
+
+    timer.append(time())#t7
+
+    for i in range(0,len(points)):#0.0019
 
         x1=int(points[i][0]-points[i][2])
         x2=int(points[i][0]+points[i][2])
@@ -87,11 +83,19 @@ for k in range(100):
         colour=int(2.55*points[i][3])
         draw.ellipse((x1,y1,x2,y2),fill=(colour,colour,colour))
 
-    disp.display()
-    end=time()
-    print(end-start)
+    timer.append(time())#t8
 
+    disp.display()#0.13
 
+    timer.append(time())#t9
+
+    print(points)
+
+    for t in range(0,len(timer)-1):
+        print(timer[t+1]-timer[t])
+
+    print('total')
+    print(timer[len(timer)-1]-timer[0])
 
 camera.stop_preview()
 camera.close()
