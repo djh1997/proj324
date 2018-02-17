e56b6f8247c0a3e5a36e4e7ed087364bc26ebf1e
jo <djh1997@hotmail.com>
2018-02-17 16:44:12 +0000

    remove blob detection debug

--- design/vision/shades.py
+++ design/vision/shades.py
@@ -11,7 +11,6 @@ from time import sleep, time
 from skimage.feature import blob_doh  # blob detection
 from skimage.io import imread  # convert jpg to np array
 
-import matplotlib.pyplot as plt  # for showing the blobs on image
 
 WIDTH = 128
 HEIGHT = 160
@@ -128,27 +127,6 @@ def sandd():
             disp.display()  # 0.13
 
             timer.append(time())  # t9
-
-            # blobs_list = [blobs_doh,blobs_doh]
-            # colors = ['red','lime']
-            # titles = ['Determinant of Hessian','doh']
-            # sequence = zip(blobs_list, colors, titles)
-            #
-            # fig, axes = plt.subplots(1, 2, figsize=(9, 3), sharex=True, sharey=True,
-            #                          subplot_kw={'adjustable': 'box-forced'})
-            # ax = axes.ravel()
-            #
-            # for idx, (blobs, color, title) in enumerate(sequence):
-            #     ax[idx].set_title(title)
-            #     ax[idx].imshow(img1, interpolation='nearest')
-            #     for blob in blobs:
-            #         y, x, r = blob
-            #         c = plt.Circle((x, y), r, color=color, linewidth=2, fill=False)
-            #         ax[idx].add_patch(c)
-            #     ax[idx].set_axis_off()
-            #
-            # plt.tight_layout()
-            # plt.show()
 
             # print(points)
             #
