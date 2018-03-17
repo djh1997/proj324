96117bc02836caf5cd8efa64997e18441c9025a1
jo <djh1997@hotmail.com>
2018-03-17 13:39:13 +0000

    comments

--- design/code/shades.py
+++ design/code/shades.py
@@ -86,7 +86,7 @@ def initcamera():
                          int(128 * scaleFactor))  # set resolution to screens
     camera.rotation = 270  # correct orinetation
     camera.vflip = True
-    if debug == 1:  # in-case debug was called before camera initialization was run
+    if debug == 1:  # incase debug was called before camera initialization was run
         camera.start_preview()  # start camera preview
     sleep(3)  # wait for camera to stabilise
     print 'camera initialized'
@@ -115,11 +115,11 @@ def debugset():
 def runningstateset(state):
     global running
     try:  # try assuming state is a button
-        if state.pin.number == 14 and state.is_held:  # if button 4 is held
+        if state.pin.number == 14 and state.is_held:  # if state button is held
             state = 1  # set state to running
-        elif state.pin.number == 14:  # if button 4 is pressed
+        elif state.pin.number == 14:  # if state button is pressed
             state = 0  # set state to stopped
-        elif state.pin.number == 15 and state.is_held:  # if button 5 is pressed
+        elif state.pin.number == 15 and state.is_held:  # if exit button is held
             state = 2  # set state to exit
     except AttributeError:  # catch not button error
         print 'not button'  # print warning
@@ -143,30 +143,31 @@ def tintButton(buttonTint):
         tintbuttonvar = 256  # set tint to clear
     elif tintbuttonvar >= 64:  # increment tint if not at limit
         tintbuttonvar -= 64
-    else:  # else must be at limit so set to clear
+    else:  # else must be at limit so set to clear ???????
         tintbuttonvar = 256  # set tint to clear
-    tintBack = [tintbuttonvar, tintbuttonvar, tintbuttonvar]
+    tintBack = [tintbuttonvar, tintbuttonvar,
+                tintbuttonvar]  # format tint correctly
     print tintBack
 
 
 def modeset(modevar):  # 0 manual 1 tint 2 point 3 auto 4 increment
     global mode, averageFps
-    try:
-        if modevar.pin.number == 3 and modevar.is_held:
-            modevar = 0
-        elif modevar.pin.number == 3:
-            if mode >= 3:
+    try:  # try assuming modevar is a button
+        if modevar.pin.number == 3 and modevar.is_held:  # if mode button is held
+            modevar = 0  # reset mode to manual
+        elif modevar.pin.number == 3:  # if mode button is pressed
+            if mode >= 3:  # if at limit the reset to manual ??????
                 modevar = 0
-            else:
+            else:  # else increment
                 modevar = mode + 1
-    except AttributeError:
-        print 'not button'
-    mode = modevar
-    print 'mode ' + str(mode)
-    averageFps = []
+    except AttributeError:  # catch not button error
+        print 'not button'  # print warning
+    mode = modevar  # set mode
+    print 'mode ' + str(mode)  # display new mode
+    averageFps = []  # reset average fps array
 
 
-def runningstateget():
+def runningstateget():  # function for telegram to retrieve current state
     global running
     return running
 
@@ -174,23 +175,24 @@ def runningstateget():
 def getiso():
     global camera
     maxtint = 4
-    iso = float(camera.analog_gain)
-    iso = (iso * maxtint)
-    iso = (256 - (maxtint * 8)) + iso
+    iso = float(camera.analog_gain)  # get current ambient brightness 0..8
+    iso = (iso * maxtint)  # adjust buy max tint level
+    iso = (256 - (maxtint * 8)) + iso  # clear - max tint + ISO tint
     return int(iso)
 
 
 def halt():
-    os.system('sudo halt')
+    os.system('sudo halt')  # send halt command to terminal
 
 
 def initbuttons():
-    buttonTint.wait_for_release()
+    buttonTint.wait_for_release(
+    )  # wait incase any of the buttons are locked high
     buttonMode.wait_for_release()
     buttonDebug.wait_for_release()
     buttonReset.wait_for_release()
     buttonexit.wait_for_release()
-    buttonTint.when_pressed = tintButton
+    buttonTint.when_pressed = tintButton  # set button state to correct function
     buttonTint.when_held = tintButton
     buttonMode.when_pressed = modeset
     buttonMode.when_held = modeset
@@ -203,48 +205,53 @@ def initbuttons():
 
 def sandd():
     global averageFps
-    initlcd()
+    initlcd()  # initialize peripherals
     initbuttons()
     initcamera()
-    while running != 2:
-        while running == 1:
-            timer = []
+    while running != 2:  # exit state
+        while running == 1:  # running state
+            timer = []  # reset internal variables
             points = []
             modeinternal = mode
 
-            if (modeinternal == 1) or (modeinternal == 3):
-                timer.append(time())
-                ti = getiso()
-                tintBackset([ti, ti, ti])
+            if (modeinternal == 1) or (
+                    modeinternal == 3):  # if mode with auto background tint
+                timer.append(time())  # add timer point
+                ti = getiso()  # get light level
+                tintBackset([ti, ti, ti])  # calculate background tint
 
-            if modeinternal >= 2:
-                timer.append(time())
+            if modeinternal >= 2:  # if active point shading mode
+                timer.append(time())  # add timer point
 
                 camera.capture(
-                    'image1.jpg', use_video_port=True, thumbnail=None)
+                    'image1.jpg', use_video_port=True,
+                    thumbnail=None)  # capture image
 
-                timer.append(time())
+                timer.append(time())  # add timer point
                 img1 = imread('image1.jpg', as_grey=True)
 
-                timer.append(time())
-                blobs_doh = blob_doh(img1, max_sigma=15, threshold=.0075)
+                timer.append(time())  # add timer point
+                blobs_doh = blob_doh(
+                    img1, max_sigma=15, threshold=.0075)  # find blobs
 
-                timer.append(time())
+                timer.append(time())  # add timer point
 
-                for i in range(len(blobs_doh)):
+                for i in range(len(blobs_doh)):  # calculate points from blobs
                     points.append([
                         blobs_doh[i][0] / scaleFactor,
                         blobs_doh[i][1] / scaleFactor,
                         (blobs_doh[i][1] / 3) / scaleFactor, tintShade
-                    ])
+                    ])  # x,y,r,tint
 
-            timer.append(time())
+            timer.append(time())  # add timer point
 
-            disp.clear((tintBack[2], tintBack[1], tintBack[0]))
+            disp.clear((tintBack[2], tintBack[1],
+                        tintBack[0]))  # set background tint
 
-            if modeinternal >= 2:
-                timer.append(time())
-                for i in range(0, len(points)):
+            if modeinternal >= 2:  # if active point shading mode
+                timer.append(time())  # add timer point
+                for i in range(0,
+                               len(points)):  # convert x,y,r to bounding box
                     x1 = int(points[i][0] - points[i][2])
                     x2 = int(points[i][0] + points[i][2])
                     y1 = int(points[i][1] - points[i][2])
@@ -252,34 +259,39 @@ def sandd():
                     draw.ellipse(
                         (x1, y1, x2, y2),
                         fill=(points[i][3][2], points[i][3][1],
-                              points[i][3][0]))
+                              points[i][3][0]))  # draw
 
-            timer.append(time())
+            timer.append(time())  # add timer point
 
-            disp.display()
+            disp.display()  # put the drawing on the the LCD's
 
-            timer.append(time())
+            timer.append(time())  # add timer point
 
-            if debug == 1:
+            if debug == 1:  # if debug is on the print timings
 
                 print 'number of points: {}\n'.format(len(points))
                 print 'background tint: {}\n'.format(tintBack)
                 print 'foreground tint: {}\n'.format(tintShade)
-                for t in range(0, len(timer) - 1):
+                for t in range(0,
+                               len(timer) -
+                               1):  # iterate over timing list to get timings
                     print 'function {} : time {}\n'.format(
-                        processpoint[modeinternal][t], timer[t + 1] - timer[t])
+                        processpoint[modeinternal][t],
+                        timer[t + 1] - timer[t])  # print function and time
 
-                totaltime = timer[len(timer) - 1] - timer[0]
-                averageFps.append(1 / totaltime)
-                if len(averageFps) >= 11:
-                    if len(averageFps) >= 61:
+                totaltime = timer[len(timer)
+                                  - 1] - timer[0]  # calculate total time
+                averageFps.append(1 / totaltime)  # calculate fps and append
+                if len(averageFps) >= 11:  # buffer has min number of values
+                    if len(averageFps) >= 61:  # buffer full then remove oldest
                         averageFps.pop(0)
                     print 'average fps = {: 4.2f}\n'.format(
-                        sum(averageFps) / len(averageFps))
+                        sum(averageFps) / len(averageFps))  # print average fps
                 print 'total = {: 4.2f}  fps = {: 4.2f}\n'.format(
-                    totaltime, averageFps[len(averageFps) - 1])
+                    totaltime, averageFps[len(averageFps)
+                                          - 1])  # print total time and fps
 
         sleep(1)
 
-    deinitlcd()
+    deinitlcd()  # close peripherals
     deinitcamera()
